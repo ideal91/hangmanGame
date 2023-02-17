8021a5bc13aea8f215afd357aed34212d37cc752
Ideal Bacaj <bacaj_ideal@yahoo.com>
2023-02-17 00:19:10 +0000

    first commit

---
+++ react-hangman-master/src/App.js
@@ -0,0 +1,16 @@
+//https://www.youtube.com/watch?v=MwBO1HUEoR0
+//The above video has been very helpful to find more information
+
+import React from "react";
+import Hangman from "./Hangman";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Hangman />
+    </div>
+  );
+}
+
+export default App;
